d35dbbf79d66b71c8071e4dd3e500caa82170667
MysikM <mishamysik@gmail.com>
2023-09-08 15:29:02 +0300

    add config

--- src/hooks/useErrorNotification.ts
+++ src/hooks/useErrorNotification.ts
@@ -1,9 +1,11 @@
 import { useSnackbar } from 'notistack';
+import { useTranslation } from 'react-i18next';
 
 export const useErrorNotification = () => {
 	const { enqueueSnackbar } = useSnackbar();
+	const { t } = useTranslation('settings');
 	const errorNotification = (message?: string) => {
-		enqueueSnackbar(message ? message : 'Помилка виконання запиту', {
+		enqueueSnackbar(message ? message : t('requestExecutionError'), {
 			variant: 'error',
 			anchorOrigin: {
 				vertical: 'top',
